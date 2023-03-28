c206a3cd5697dd96bf133146506ffebeeead91b1
chanjure <chanjure@snu.ac.kr>
2023-03-28 16:06:00 +0100

    plotcounts.py takes argument

--- bin/plotcounts.py
+++ bin/plotcounts.py
@@ -2,9 +2,23 @@
 
 import pandas as pd
 import matplotlib.pyplot as plt
+import argparse
 
+parser = argparse.ArgumentParser(description=(
+  "Plot word counts."
+  ))
+parser.add_argument('infile', type=argparse.FileType('r'),
+                    nargs='?', default='-',
+                    help='Input file name')
+parser.add_argument('outfile', type=str,
+                    default=None,
+                    help='Output file name')
+parser.add_argument('-x', '--xlim', type=float, nargs=2,
+                    metavar=('XMIN','XMAX'), default=None,
+                    help="xlim of the plot")
+args = parser.parse_args()
 
-df = pd.read_csv('results/dracula.csv', header=None,
+df = pd.read_csv(args.infile, header=None,
                  names=('word', 'word_frequency'))
 df['rank'] = df['word_frequency'].rank(ascending=False,
                                        method='max')
@@ -13,6 +27,6 @@ ax = df.plot.scatter(x='word_frequency',
                      y='inverse_rank',
                      figsize=[12, 6],
                      grid=True,
-                     xlim=None)
+                     xlim=args.xlim)
 #plt.show()
-plt.savefig('results/dracula.pdf')
+plt.savefig(args.outfile)
